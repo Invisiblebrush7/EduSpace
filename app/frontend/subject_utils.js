ef3620b6636f8e49c0fd3faa1fa2071565a06ce6
David Naranjo <davidabrahamns@gmail.com>
2022-05-10 23:37:43 -0500

    Utils modificado

--- app/frontend/subject_utils.js
+++ app/frontend/subject_utils.js
@@ -1,37 +1,29 @@
 const productsURL = 'http://localhost:3000/products';
 
-function productToHTML(subject) {
-    // this.name = name;
-    // this.description = description;
-    // this.requiresOneBefore = requiresOneBefore;
-    // this.teacherID = teacherID;
-    // this.id = 1;
+function teachersToHTML(subject) {
+
     let name = subject.name;
     let description = subject.description;
     let requiresOneBefore = subject.requiresOneBefore;
     let teacherID = subject.teacherID;
     let id = subject.id;
 
-    let productHTML = ``;
-    return productHTML;
-}
+    console.log(subject);
 
-function productListToHTML(products) {
-    let productsAsHTML = '';
-    products.forEach((product) => {
-        productsAsHTML += productToHTML(product);
-    });
-    let mainList = `
-    <div id="teacherCard">
-		<div class="d-flex justify-content-start-sm" style="border: 1px solid #000">
-			<button id="circle"></button>
-			<div class="col-md-6 col-sm-3">
-				<p style="margin-top: 20px; margin-left: 5px">Profesor(a)</p>
-				<p style="margin-left: 5px; font-size: 30px">${productToHTML.teacherID.name}</p>
-				<p style="margin-left: 5px; font-size: 18px">${productToHTML.name}</p>
+    let teacherHTML = `
+        
+		<div id="teacherCard">
+			<div class="d-flex justify-content-start-sm" style="border: 1px solid #000">
+				<button id="circle"></button>
+				<div class="col-md-6 col-sm-3">
+					<p style="margin-top: 20px; margin-left: 5px">Profesor(a)</p>
+					<p style="margin-left: 5px; font-size: 30px">${teacherHTMLToHTML.teacherID.name}</p>
+					<p style="margin-left: 5px; font-size: 18px">${teacherHTMLToHTML.name}</p>
+				</div>
 			</div>
 		</div>
-	</div>
-    `;
-    return productsAsHTML;
+		`;
+
+    return teacherHTMLHTML;
+
 }
