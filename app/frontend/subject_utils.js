f023152f8280879b5984e541c56c1b202791849f
David Naranjo <davidabrahamns@gmail.com>
2022-05-11 20:35:00 -0500

    MOdificicaion de html y formularios

--- app/frontend/subject_utils.js
+++ app/frontend/subject_utils.js
@@ -1,34 +1,25 @@
 const subjectsURL = 'http://localhost:3000/materias/get';
 
 function subjectToHTML(subject) {
-	let comments = '';
+    let comments = '';
 
-	subject.comments.forEach((comment) => {
-		comments += `<li>${comment}</li>`;
-	});
+    //subject.comments.forEach((comment) => {
+    //    comments += `<li>${comment}</li>`;
+    //});
 
-	let teacherHTML = `
-		<div id="teacherCard">
-			<div class="d-flex justify-content-start-sm" style="border: 1px solid #000">
-				<button id="circle"></button>
-				<div class="col-md-6 col-sm-3">
-					<p style="margin-top: 20px; margin-left: 5px">Profesor(a)</p>
-					<p style="margin-left: 5px; font-size: 30px">${subject.name}</p>
-					<p style="margin-left: 5px; font-size: 18px">${subject.teacher_name}</p>
-					${comments}
-				</div>
-			</div>
-		</div>
+    let teacherHTML = `
+		<td>${subject.name}</td>
+      	<td><p style="margin-left: 5px; font-size: 20px">${subject.description}</td>
 		`;
-	return teacherHTML;
+    return teacherHTML;
 }
 
 function subjectsListToHTML(subjects) {
-	let mainList = '';
+    let mainList = '';
 
-	subjects.forEach((subject) => {
-		mainList += subjectToHTML(subject);
-	});
+    subjects.forEach((subject) => {
+        mainList += subjectToHTML(subject);
+    });
 
-	return mainList;
+    return mainList;
 }
