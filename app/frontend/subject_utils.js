f3f969b1b5b382bd7405b1feb58c64db34e84962
alberto romano <aromanog1@gmail.com>
2022-05-11 22:15:05 -0500

    Merging

--- app/frontend/subject_utils.js
+++ app/frontend/subject_utils.js
@@ -2,7 +2,6 @@ const subjectsURL = 'http://localhost:3000/materias/get';
 
 function subjectToHTML(subject) {
 	let comments = '';
-
 	subject.comments.forEach((comment) => {
 		comments += `<li>${comment}</li>`;
 	});
@@ -12,9 +11,12 @@ function subjectToHTML(subject) {
 			<div class="d-flex justify-content-start-sm" style="border: 1px solid #000">
 				<button id="circle"></button>
 				<div class="col-md-6 col-sm-3">
-					<p style="margin-top: 20px; margin-left: 5px">Profesor(a)</p>
 					<p style="margin-left: 5px; font-size: 30px">${subject.name}</p>
 					<p style="margin-left: 5px; font-size: 18px">${subject.teacher_name}</p>
+
+					<a href="/materias/delete/${subject.id}/${subject.teacherID}">
+						<i class="fa-solid fa-trash"></i>
+					</a>  
 					${comments}
 				</div>
 			</div>
--- app/routes/subject_routes.js
+++ app/routes/subject_routes.js
@@ -2,27 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
-const dataHandler = require('../controllers/dataHandler');
-const dataHandlerTeacher = require('../controllers/dataHandlerTeacher');
 
-const SubjectSchema = require('../models/subjectModel');
 const TeacherSchema = require('../models/teacherModel');
-
-// router.route('/get').get((req, res) => {
-// 	let subjects = undefined;
-// 	let teachers = [];
-// 	if (req.query.filter === undefined) {
-// 		subjects = dataHandler.getSubjects();
-// 		subjects.forEach((subject) => {
-// 			let teacher = dataHandlerTeacher.getTeacherByID(subject._teacherID);
-// 			teachers.push(teacher);
-// 		});
-// 	} else {
-// 		subjects = dataHandler.findSubject(req.query.filter);
-// 	}
-// 	if (subjects !== undefined) res.status(200).json([subjects, teachers]);
-// 	else res.status(400).send('Error');
-// });
 
 router.route('/get').get(async (req, res) => {
 	try {
@@ -33,12 +14,39 @@ router.route('/get').get(async (req, res) => {
 		teachers.forEach((teacher) => {
 			teacher.subjects.forEach((subject) => {
 				subject.teacher_name = teacher.name;
+				subject.teacherID = teacher.id;
 				subjects.push(subject);
 			});
 		});
 
 		if (!teachers && !subjects) throw Error('No subjects found');
 		res.status(200).json(subjects);
+	} catch (err) {
+		res.status(400).json({ msg: err });
+	}
+});
+
+router.route('/delete/:id/:teacher_id').get(async (req, res) => {
+	try {
+		let teacher = await TeacherSchema.findById(req.params.teacher_id);
+		if (!teacher) throw Error('No teachers found');
+
+		let subjects = [];
+
+		teacher.subjects.forEach((subject) => {
+			let id = req.params.id;
+
+			if (req.params.id !== id) {
+				subjects.push(subject);
+			}
+		});
+
+		await TeacherSchema.updateOne({ id: req.params.teacher_id }, { $unset: { subjects: '' } });
+		// await TeacherSchema.updateOne({ id: req.params.teacher_id }, { $set: { subjects: subjects } });
+		teacher = await TeacherSchema.findById(req.params.teacher_id);
+
+		// res.redirect('/materias');
+		res.status(200).json(teacher);
 	} catch (err) {
 		res.status(400).json({ msg: err });
 	}
